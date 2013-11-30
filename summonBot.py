47acc8977329620980bb0895712ced3054c5eb3e
Ritwik <ritwik@ritwik-linlt.(none)>
2013-11-30 12:57:55 -0800

    Added base files.

---
+++ summonBot.py
@@ -0,0 +1,24 @@
+import praw
+from shlex import split
+
+redditHandle = praw.Reddit(user_agent = "User Summon Bot")
+
+redditHandle.login('usersummonbot', 'summonbot123')
+
+testSubs = redditHandle.get_subreddit('summonbottest').get_hot(limit=100)
+
+for post in testSubs:
+	for comment in praw.helpers.flatten_tree(post.comments):
+		if ("/u/" in str(comment)):
+			print "User paged."
+			users = []	
+			splitCom = split(str(comment))
+			for item in splitCom:
+				if ("/u/" in item):
+					users.append(item[3:])
+			for user in users:
+				comment.reply("User " + user + " paged.")
+				print comment.id
+				redditHandle.send_message(user, "You have been paged.", "test.")
+				
+
